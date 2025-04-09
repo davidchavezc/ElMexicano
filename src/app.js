fcfd726eb5459cbce2542096afde1f190d22a6b6
Diego <diegoduran7647@gmail.com>
2025-04-09 11:45:53 -0600

    Configure el puerto en 3000 desde config para que se pueda importar y un npm start en el json

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import {port} from './config.js'
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
-const port = 3000;
+
 
 app.use(express.static(__dirname + "/public"));
 
