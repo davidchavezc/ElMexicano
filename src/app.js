3bffd8b3dd27310c3794b2c1c4c894ba53bfa72b
DiegoEdu19 <diegoduran7647@gmail.com>
2025-04-12 13:58:28 -0700

    Agregue el codigo en routers para que me tire los usuarios y agregue usuarios a la db ademas de que importe el router a la app.js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import {port} from './config.js'
+import {port} from './config.js';
+import router from "./routes/users.routes.js";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 
 
 app.use(express.static(__dirname + "/public"));
+app.use(router);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,5 +1,12 @@
 import {Router} from "express";
-import {pool} from '../db.js'
+import {pool} from '../db.js';
 
 const router = Router();
 
+router.get("/usuarios", async (req, res) => {
+    const result= await pool.query("SELECT * FROM usuario");
+    console.log(result);
+    
+});
+
+export default router;
