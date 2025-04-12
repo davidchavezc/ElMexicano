d7c3aade47d2120209abd3a4b960b5e1ba4c1871
DiegoEdu19 <diegoduran7647@gmail.com>
2025-04-12 14:00:12 -0700

    Cambie el nombre del router

--- src/app.js
+++ src/app.js
@@ -3,13 +3,14 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import {port} from './config.js';
 import router from "./routes/users.routes.js";
+import routerUsuarios from "./routes/users.routes.js";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 
 
 app.use(express.static(__dirname + "/public"));
-app.use(router);
+app.use(routerUsuarios);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
