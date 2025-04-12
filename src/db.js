fa83472b4c25c4adbbac3a14605f3f2ca1c10f06
DiegoEdu19 <diegoduran7647@gmail.com>
2025-04-11 22:19:53 -0700

    Agregue codigo a db.js para crear el pool que se conecta a la bd

---
+++ src/db.js
@@ -0,0 +1,9 @@
+import pg from 'pg'
+
+export const pool = new pg.Pool({
+    user: "postgres", //usuario que tengas en postgre
+    host: "localhost",
+    password: "", //password que utilizas en postgre
+    database: "ElMexicano", // nombre de la database
+    port: "5432"
+})
