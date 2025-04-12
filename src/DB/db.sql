25fbf1d5161c3243e2c0dc7de223624a268b9f29
DiegoEdu19 <diegoduran7647@gmail.com>
2025-04-12 14:41:38 -0700

    Elimine las lineas innecesarias en db.sql y solo deje los cambios que he implementado en la db para que funcione lo que llevo hasta ahora

--- src/DB/db.sql
+++ src/DB/db.sql
@@ -1,22 +1,7 @@
-CREATE TABLE public.rol (
-    id_rol serial PRIMARY KEY NOT NULL,
-    nombre_tipo text NOT NULL,
-    CONSTRAINT rol_pkey PRIMARY KEY (id_rol)
-);
+
 INSERT INTO rol (nombre_tipo) 
 VALUES ('Administrador'),('empleado');
 
-
-
-CREATE TABLE public.usuario (
-    id_empleado serial PRIMARY KEY NOT NULL,
-    nombre_empleado text NOT NULL,
-    apellido_empleado text NOT NULL,
-    usuario_empleado text NOT NULL,
-    contraseña_empleado text NOT NULL,
-    id_rol integer NOT NULL,
-    CONSTRAINT usuario_pkey PRIMARY KEY (id_empleado)
-);
 
 INSERT INTO usuario 
 (nombre_empleado,apellido_empleado, usuario_empleado,contrasena_empleado,id_rol)
