428251c4a1bfe499c3ffb1b7d507d4351389fa3b
DiegoEdu19 <diegoduran7647@gmail.com>
2025-04-12 14:35:05 -0700

    Agregue path a app.js y cambie las rutas porque no funcionaba la app al inicializar

--- src/app.js
+++ src/app.js
@@ -1,16 +1,22 @@
 import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import path from "path";
 import {port} from './config.js';
-import router from "./routes/users.routes.js";
 import routerUsuarios from "./routes/users.routes.js";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 
 
-app.use(express.static(__dirname + "/public"));
-app.use(routerUsuarios);
+app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+app.use("/" , routerUsuarios);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
