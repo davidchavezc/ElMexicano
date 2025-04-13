fc9bde22ff85bb2a097496bd3bfdb4f200a5dddf
DiegoEdu19 <diegoduran7647@gmail.com>
2025-04-13 13:08:47 -0700

    Agregue el get de marcas en los controllers y lo importe al router

--- src/controllers/marca.controllers.js
+++ src/controllers/marca.controllers.js
@@ -1,6 +1,13 @@
 import {pool} from '../db.js'
 
 export const getMarcas = async (req,res) => {
-    
+    try{
+        const result = await pool.query("SELECT * FROM marcas");
+        res.json(result.rows); 
+    }
+    catch{
+        console.error("Error al obtener marcas:", error);
+        res.status(500).send("Error interno del servidor");
+    }
 
 }
--- src/routes/marca.routes.js
+++ src/routes/marca.routes.js
@@ -1,5 +1,8 @@
 import {Router} from "express";
+import { getMarcas } from "../controllers/marca.controllers";
 
 const routerMarcas = Router();
+
+routerMarcas.get('/marcas', getMarcas);
 
 export default routerMarcas;
