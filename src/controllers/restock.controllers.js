081f9d71c485522d5c96e3261d4d52f40aae64f0
DiegoEdu19 <diegoduran7647@gmail.com>
2025-04-16 13:22:12 -0700

    Agregue el codigo para que funcione el controller y route de restock

---
+++ src/controllers/restock.controllers.js
@@ -0,0 +1,13 @@
+import {pool} from '../db.js'
+
+export const getPiezas = async (req,res) => {
+    try{
+        const result = await pool.query("SELECT * FROM piezas");
+        res.json(result.rows); 
+    }
+    catch{
+        console.error("Error al obtener piezas:", error);
+        res.status(500).send("Error interno del servidor");
+    }
+
+}
