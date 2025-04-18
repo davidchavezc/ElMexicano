3502a71ebf979121ddb4bed685ed10c6085d1c5f
davidchavezc <177746616+davidchavezc@users.noreply.github.com>
2025-04-17 23:06:08 -0600

    Añadir rutas y controlador para categorias

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import routerUsuarios from "./routes/usuarios.routes.js";
 import routerMarcas from "./routes/marca.routes.js";
 import routerVenta from "./routes/venta.routes.js";
 import routerRestock from "./routes/restock.routes.js";
+import routerCategorias from "./routes/categorias.routes.js";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
@@ -19,10 +20,16 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-app.use("/" , routerUsuarios);
+app.get("/admin", (req, res) => {
+  res.sendFile(path.join(__dirname, "../admin/reporte.html"));
+});
+
+app.use("/", routerUsuarios);
 app.use("/", routerMarcas);
 app.use("/", routerVenta);
 app.use("/", routerRestock);
+app.use("/", routerCategorias);
+
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
