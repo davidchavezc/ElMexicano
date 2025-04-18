f935fbe92e5b0d60d872a18379be4448a1e1e43f
davidchavezc <177746616+davidchavezc@users.noreply.github.com>
2025-04-17 20:22:36 -0600

    Credenciales de incio de sesion removidads

--- src/db.js
+++
@@ -1,9 +0,0 @@
-import pg from 'pg';
-
-export const pool = new pg.Pool({
-    user: "postgres", //usuario que tengas en postgre
-    host: "localhost",
-    password: "Superman19", //password que utilizas en postgre
-    database: "ElMexicano", // nombre de la database
-    port: "5432"
-})
