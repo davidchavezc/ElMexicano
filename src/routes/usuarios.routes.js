11bad5bbf70892768c65ba2396ce041257993f79
nestorgarciac <nestorgarciac19@gmail.com>
2025-04-18 16:29:04 -0600

    Se agrego el endpoint en el archivo usuarios.router.js para agregar usuarios

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -4,6 +4,10 @@ import { getUsuarios } from "../controllers/usuarios.controllers.js";
 
 const routerUsuarios = Router();
 
+//Endpoint para obtener a los usuarios 
 routerUsuarios.get('/usuarios', getUsuarios);
+
+// Endpoint para agregar un usuario
+routerUsuarios.post("/usuarios", agregarUsuario);
 
 export default routerUsuarios;
