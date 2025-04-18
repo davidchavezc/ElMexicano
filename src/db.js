0bcf17b09a23f39eef28cbb4d9fa80a740db0e9d
davidchavezc <177746616+davidchavezc@users.noreply.github.com>
2025-04-17 21:32:11 -0600

    Añadir variables de entorno

--- src/db.js
+++ src/db.js
@@ -1,9 +1,12 @@
-import pg from 'pg'
+import pg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const pool = new pg.Pool({
-    user: "", //usuario que tengas en postgre
-    host: "localhost",
-    password: "", //password que utilizas en postgre
-    database: "", // nombre de la database
-    port: "5432"
-})
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST, 
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+});
