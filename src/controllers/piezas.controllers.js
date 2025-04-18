4f644c0a663c24ec5778d336cf97486144fd6aea
davidchavezc <177746616+davidchavezc@users.noreply.github.com>
2025-04-18 00:31:16 -0600

    Añadido controlador y rutas de piezas

---
+++ src/controllers/piezas.controllers.js
@@ -0,0 +1,12 @@
+import { pool } from '../db.js'
+
+export const getPiezas = async (req, res) => {
+    try{
+        const result = await pool.query("SELECT * FROM pieza");
+        res.json(result.rows);
+    }
+    catch{
+        console.error("Error al obtener pieza:", error);
+        res.status(500).send("Error interno del servidor");
+    }
+}
