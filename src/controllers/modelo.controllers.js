8bdab22ea4b813146c27ad0aeba8c559c940203c
Leixhik <eduardo.leijapl@uanl.edu.mx>
2025-04-18 21:40:39 -0600

    creación de GEt por ID

--- src/controllers/modelo.controllers.js
+++ src/controllers/modelo.controllers.js
@@ -10,3 +10,20 @@ export const getModelos = async (req, res) => {
     res.status(500).json("Error al obtener modelos del servidor");
   }
 };
+
+// Obtener modelo por ID
+export const getModeloPorId = async (req, res) => {
+    try{
+        const { id } = req.params;
+        const result = await pool.query("SELECT * FROM modelo WHERE id_modelo $1", [id]);
+        
+        if(result.rows.lenght === 0){
+            return res.status(404).json({ message: "Modelo no encontrado"});
+        }
+        
+        res.json(result.rows[0]);
+    }catch(error){
+        console.error("Error al obtener modelo: ", error);
+        res.status(500).json({message: "Error al obtener modelo"});
+    }
+};
