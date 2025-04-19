20197c02a4b426183d94d378c71335057926aed5
Leixhik <eduardo.leijapl@uanl.edu.mx>
2025-04-18 22:41:45 -0600

    creación de ROUTER modelo

---
+++ src/routes/modelo.routes.js
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import {
+    getModelos,
+    getModeloPorId,
+    updateModelo,
+    deleteModelo,
+    getModelosPorMarca,
+    crearModelo
+} from "../controllers/modelo.controllers.js";
+
+const router = Router();
+
+// Rutas para modelos
+router.get("/modelos", getModelos);
+router.get("/modelos/:id", getModeloPorId);
+router.post("/modelos", crearModelo);
+router.put("/modelos/:id", updateModelo);
+router.delete("/modelos(:id", deleteModelo);
+router.get("(modelos/marca/:id_marca", getModeloPorId);
+
+export default router;
