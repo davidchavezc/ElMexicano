1d31d32de1e93b92d171103e2e46b4e93704b465
davidchavezc <177746616+davidchavezc@users.noreply.github.com>
2025-04-20 17:39:50 -0600

    Eliminar restock controller y routes
    Se eliminan porque sus funcionalidades fueron
    pasadas a un end-point en piezas, y se volvió
    redundante.

--- src/routes/pieza.routes.js
+++ src/routes/pieza.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getPiezaById, getPiezas } from "../controllers/piezas.controllers.js";
+import { getPiezaById, getPiezas, updatePiezas } from "../controllers/piezas.controllers.js";
 
 const routerPiezas = Router();
 
 routerPiezas.get('/Piezas', getPiezas);
 routerPiezas.get('/Piezas/:id', getPiezaById);
+routerPiezas.put('/piezas', updatePiezas);
 
 export default routerPiezas;
