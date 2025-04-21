9e61663b4b78d36d7f2eb5e46bdec80c82819130
Leixhik <eduardo.leijapl@uanl.edu.mx>
2025-04-21 17:10:34 -0600

    Fix pequeño a sintaxis de modelos.html + modelo.controllers, dejé en blanco el path de db.js

--- src/controllers/modelo.controllers.js
+++ src/controllers/modelo.controllers.js
@@ -2,13 +2,13 @@ import { pool } from "../db.js";
 
 // Obtener todos los modelos
 export const getModelos = async (req, res) => {
-  try {
-    const result = await pool.query("SELECT * FROM modelos");
-    res.json(result.rows);
-  } catch (error) {
-    console.error("Error al obtener modelos:", error);
-    res.status(500).json("Error al obtener modelos del servidor");
-  }
+    try {
+        const result = await pool.query("SELECT * FROM modelos");
+        res.json(result.rows);
+    } catch (error) {
+        console.error("Error al obtener modelos:", error);
+        res.status(500).json({ message: "Error al obtener modelos" });
+    }
 };
 
 // Obtener modelo por ID
@@ -17,7 +17,7 @@ export const getModeloPorId = async (req, res) => {
         const { id } = req.params;
         const result = await pool.query("SELECT * FROM modelos WHERE id_modelo = $1", [id]);
         
-        if(result.rows.lenght === 0){
+        if(result.rows.length === 0){
             return res.status(404).json({ message: "Modelo no encontrado"});
         }
         
