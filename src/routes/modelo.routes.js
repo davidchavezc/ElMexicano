3ab5255b1776f1a66ec42245a32a7cfbac1f8ab3
Leixhik <eduardo.leijapl@uanl.edu.mx>
2025-04-21 16:04:11 -0600

    modificación de routers de modelo

--- src/routes/modelo.routes.js
+++ src/routes/modelo.routes.js
@@ -15,7 +15,7 @@ router.get("/modelos", getModelos);
 router.get("/modelos/:id", getModeloPorId);
 router.post("/modelos", crearModelo);
 router.put("/modelos/:id", updateModelo);
-router.delete("/modelos(:id", deleteModelo);
-router.get("(modelos/marca/:id_marca", getModelosPorMarca);
+router.delete("/modelos/:id", deleteModelo);
+router.get("/modelos/marca/:id_marca", getModelosPorMarca);
 
 export default router;
