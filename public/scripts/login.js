303130f9b8d4749f94fb3a5a88b4b94b7273873e
DiegoEdu19 <diegoduran7647@gmail.com>
2025-04-21 22:14:26 -0700

    Agregue un mensaje de error si las credenciales son incorrectas y un div en html para ese mensaje

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -6,11 +6,16 @@ document.addEventListener("DOMContentLoaded", () => {
       const user = e.target.querySelector('input[name="usuario"]')?.value.trim();
       const password = e.target.querySelector('input[name="contraseña"]')?.value.trim();
 
+      const errorMessage = document.getElementById("error-message"); // Se asegura de que el mensaje de error tenga un contenedor para mostrar
 
+      // Limpiar el mensaje de error previo
+      errorMessage.style.display = "none";
+      errorMessage.textContent = "";
 
       if (!user || !password) {
-          alert("Por favor, completa todos los campos.");
-          return;
+        errorMessage.textContent = "Por favor, completa todos los campos.";
+        errorMessage.style.display = "block"; // Muestra el mensaje
+        return;
       }
 
       try {
@@ -26,8 +31,14 @@ document.addEventListener("DOMContentLoaded", () => {
           });
 
           if (!res.ok) {
-              console.error("Error en la autenticación:", res.status);
-              return alert("Credenciales incorrectas o problema en el servidor.");
+            if (res.status === 401) {
+              const errorData = await res.json();
+              errorMessage.textContent = errorData.mensaje;
+              errorMessage.style.display = "block"; // Muestra el mensaje
+          } else {
+              alert("Hubo un problema al iniciar sesión.");
+          }
+          return;
           }
 
           const resJson = await res.json();
