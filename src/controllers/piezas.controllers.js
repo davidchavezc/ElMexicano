72de340f0010122bd7e3cb2d5f448bbc4716791e
davidchavezc <davidchavezc05@gmail.com>
2025-04-24 00:23:45 -0600

    Agregada pantalla de detalle de pieza

--- src/controllers/piezas.controllers.js
+++ src/controllers/piezas.controllers.js
@@ -27,7 +27,16 @@ export const updatePiezas = async (req,res) => {
 export const getPiezaById = async (req, res) => {
     const {id} = req.params;
     try{
-        const result = await pool.query('SELECT * FROM pieza WHERE id_pieza=$1', [id]);
+        const result = await pool.query(`
+        SELECT id_pieza AS id, nombre_pieza, pieza.id_categoria, categoria.nombre AS nombre_categoria,
+        pieza.id_modelo, modelos.nombre_modelo,
+        pieza.id_marca, marcas.nombre_marca, 
+        imagen, descripcion, cantidad, precio
+        FROM pieza 
+        INNER JOIN modelos ON pieza.id_modelo = modelos.id_modelo 
+        INNER JOIN categoria ON pieza.id_categoria = categoria.id_categoria 
+        INNER JOIN marcas ON pieza.id_marca = marcas.id_marca 
+        WHERE id_pieza=$1`, [id]);
         if (result.rows.length === 0) {
             return res.status(404).send('Pieza no encontrada');
           }
