5b37f7cd240421e2dc6ad90cff36997b69436bb5
Leixhik <eduardo.leijapl@uanl.edu.mx>
2025-04-25 17:15:46 -0600

    Se han creado los endpoints del crud de pieza

--- src/routes/pieza.routes.js
+++ src/routes/pieza.routes.js
@@ -11,3 +11,24 @@ import{
     getPiezasPorCategoria,
     getCategorias
 } from '../controllers/pieza.controllers.js';
+
+const routerDePiezas = Router();
+
+// Rutas para piezas
+routerDePiezas.get('/piezas', getObtenerPieza);
+routerDePiezas.get('/piezas:id', getObtenerPiezaPorId);
+routerDePiezas.post('/piezas', postCrearPieza);
+routerDePiezas.put('/piezas:id', actualizarPieza);
+routerDePiezas.delete('/piezas:id', eliminarPieza);
+
+// Rutas para restock
+routerDePiezas.put('/piezas/restock/:id', actualizarCantidadPieza);
+
+// Rutas para filtrado
+routerDePiezas.get('/piezas/marca/:id_marca', obtenerPiezasPorMarca);
+routerDePiezas.get('/piezas/modelo/:id_modelo', getPiezasPorModelo);
+routerDePiezas.get('/piezas/categoria/:id_categoria', getPiezasPorCategoria);
+
+// Rutas para categorias
+routerDePiezas.get('/categorias', getCategorias);
+export default routerDePiezas;
