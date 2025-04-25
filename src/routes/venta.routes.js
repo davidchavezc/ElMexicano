87735b8db5c38070a022f067aa7f882834fba521
davidchavezc <davidchavezc05@gmail.com>
2025-04-24 23:25:15 -0600

    Añadida validación en venta
    Ya no se pueden tener piezas duplicadas.

--- src/routes/venta.routes.js
+++ src/routes/venta.routes.js
@@ -1,8 +1,9 @@
 import {Router} from "express";
-import { getPiezas } from "../controllers/venta.controllers.js";
+import { getPiezaById, getPiezas } from "../controllers/venta.controllers.js";
 
 const routerVenta = Router();
 
 routerVenta.get('/ventas', getPiezas);
+routerVenta.get('/ventas/:id', getPiezaById);
 
 export default routerVenta;
