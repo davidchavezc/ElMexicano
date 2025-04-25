cc410ce5d100adf3a99e6cc62114be4e56f0d384
DiegoEdu19 <diegoduran7647@gmail.com>
2025-04-24 19:42:45 -0700

    Cambie el nombre del get piezas y modifique la funcion para Venta

--- src/controllers/venta.controllers.js
+++ src/controllers/venta.controllers.js
@@ -1,11 +1,12 @@
-import {pool} from '../db.js'
+import { pool } from '../db.js';
 
 export const getPiezas = async (req, res) => {
-    try {
+    try{
         const result = await pool.query("SELECT * FROM pieza");
-        res.json(result.rows); 
-    } catch (error) {
-        console.error("Error al obtener piezas:", error);
+        res.json(result.rows);
+    }
+    catch{
+        console.error("Error al obtener pieza:", error);
         res.status(500).send("Error interno del servidor");
     }
 };
