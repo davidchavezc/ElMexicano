b10cfc995b60771659a3ca52339ca7e5fc6b3e27
Leixhik <eduardo.leijapl@uanl.edu.mx>
2025-04-25 15:33:42 -0600

    creacion de UPDATE pero en cantidad de piezas para restock

--- src/controllers/pieza.controllers.js
+++ src/controllers/pieza.controllers.js
@@ -82,3 +82,25 @@ export const eliminarPieza = async (req, res) => {
         res.status(500).json({ message: "Error al eliminar pieza" });
     }
 };
+
+// Actualizar cantidad de piezas para restock
+export const actualizarCantidadPieza = async (req, res) => {
+    try{
+        const { id }= req.params;
+        const {cantidad} = req.body;
+
+        const result = await pool.query(
+            "UPDATE pieza SET cantidad =  cantidad + $1 WHERE id_pieza = $2 RETURNING *",
+            [cantidad, id]
+        );
+
+        if (result.rows.length === 0){
+            return res.status(404).json({ message: "Pieza no encontrada" });
+        }
+
+        res.json(result.rows[0]);
+    } catch (error){
+        console.error("Error al actualizar cantidad de piezas: ", error);
+        res.status(500).json({ message: "Error al actualizar cantidad de piezas" });
+    }
+};
