02f070734690df0c70c381c6e868184b3196ea2f
nestorgarciac <nestorgarciac19@gmail.com>
2025-05-01 20:10:40 -0600

    Corrección de la ruta en el historial

--- src/routes/historial.routes.js
+++ src/routes/historial.routes.js
@@ -1,9 +1,10 @@
+// historial.routes.js
 import { Router } from "express";
 import { obtenerHistorial, filtrarPorFecha } from "../controllers/historial.controllers.js";
 
 const routerHistorial = Router();
 
-routerHistorial.get("/", obtenerHistorial);
-routerHistorial.get("/filtrar", filtrarPorFecha);
+routerHistorial.get("/api/historial", obtenerHistorial);        
+routerHistorial.get("/api/historial/filtrar", filtrarPorFecha);  
 
 export default routerHistorial;
