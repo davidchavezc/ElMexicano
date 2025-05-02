eaca8572ef01605a5290eb67bcf3dfbece3bf1b0
davidchavezc <davidchavezc05@gmail.com>
2025-05-01 23:40:26 -0600

    Se arregló un bug al momento de iniciar la BD

--- src/DB/db.sql
+++ src/DB/db.sql
@@ -2,18 +2,6 @@
 INSERT INTO rol (nombre_tipo) 
 VALUES ('Administrador'),('empleado');
 
-
-INSERT INTO usuario 
-(nombre_empleado,apellido_empleado, usuario_empleado,contrasena_empleado,id_rol)
-VALUES
-('Diego','Duran','Diego1','hola',1);
-
-INSERT INTO usuario 
-(nombre_empleado,apellido_empleado, usuario_empleado,contrasena_empleado,id_rol)
-VALUES
-('David','Chavez','David1','hola',2);
-
-
 INSERT INTO marcas
 (nombre_marca)
 VALUES('Toyota');
@@ -23,6 +11,6 @@ INSERT INTO pieza
 VALUES('MotorCamry',1,1,1,1);
 
 INSERT INTO categoria
-(nombre_categoria)
+(nombre)
 VALUES('Motores');
 
