8204c0a52e2ac6acb3e41c99b71d47976e0856c4
davidchavezc <177746616+davidchavezc@users.noreply.github.com>
2025-05-11 21:07:52 -0600

    Mejorar starter, se remueve sample data

--- src/DB/db.sql
+++ src/DB/db.sql
@@ -2,15 +2,3 @@
 INSERT INTO rol (nombre_tipo) 
 VALUES ('Administrador'),('empleado');
 
-INSERT INTO marcas
-(nombre_marca)
-VALUES('Toyota');
-
-INSERT INTO pieza 
-(nombre_pieza,id_modelo,id_marca,id_categoria,cantidad)
-VALUES('MotorCamry',1,1,1,1);
-
-INSERT INTO categoria
-(nombre)
-VALUES('Motores');
-
