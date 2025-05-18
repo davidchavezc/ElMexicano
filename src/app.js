fa121e65dd5c24a038ff0aa84258f764c2d040d2
davidchavezc <davidchavezc05@gmail.com>
2025-05-18 16:35:44 -0600

    Nuevo log-in con passport

--- src/app.js
+++ src/app.js
@@ -12,22 +12,68 @@ import routerModelos from "./routes/modelo.routes.js";
 import routerDePiezas from "./routes/pieza.routes.js";
 import routerHistorial from "./routes/historial.routes.js";
 import routerReporte from "./routes/reporte.routes.js";
-
+import passport from "passport";
+import { pool } from './db.js';
+import { Strategy as LocalStrategy } from 'passport-local';
 
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port =3000;
 
-app.use(session({
-  secret: process.env.DB_PASSWORD,
-  resave: false,
-  saveUninitialized: false,
-  cookie: { secure: false }
-}));
+app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../public")));
+
+passport.use(
+      // console.log('Reached')
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      console.log('Reached')
+      const { rows } = await pool.query("SELECT * FROM usuario WHERE usuario_empleado = $1", [username]);
+      const user = rows[0];
+      console.log(user)
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      if (user.contrasena_empleado !== password) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch(err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id_empleado);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM usuario WHERE id_empleado = $1", [id]);
+    const user = rows[0];
+
+    done(null, user);
+  } catch(err) {
+    done(err);
+  }
+});
+
+app.get("/log-out", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
 
 // Rutas para usuarios
 app.get("/", (req, res) => {
@@ -36,6 +82,10 @@ app.get("/", (req, res) => {
 
 app.get("/login", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/main/login.html"));
+});
+
+app.get("/error", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/main/error.html"));
 });
 
 app.get("/nosotros", (req, res) => {
@@ -104,9 +154,13 @@ app.use("/", routerDePiezas);
 app.use("/", routerHistorial);
 app.use("/", routerReporte);
 
-
-
-
+app.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/admin",
+    failureRedirect: "/error"
+  })
+);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
