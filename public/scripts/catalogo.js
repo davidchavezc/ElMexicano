6805382f10ac2085e65c92838d261531e372a14a
davidchavezc <177746616+davidchavezc@users.noreply.github.com>
2025-05-25 15:38:17 -0600

    Solucionar bug en detalle
    No se mostraban correctamente los nombres de marca
    y modelo en el detalle

--- public/scripts/catalogo.js
+++ public/scripts/catalogo.js
@@ -35,7 +35,7 @@ async function desplegarPiezas(){
         const piezahtml = `
         <div class="col" class="pieza-tarjeta">
         <div class="card text-center">
-        <a href='/producto?id=${pieza.id_pieza}' class='text-decoration-none text-black'>
+        <a href='/detalle?id=${pieza.id_pieza}' class='text-decoration-none text-black'>
         <img src="/img/${pieza.id_categoria}.png" class="card-img-top" alt="${pieza.nombre_pieza}">
         <div class="card-body">
         <h6 class="card-title">${pieza.nombre_pieza}</h6>
--- public/scripts/detalle.js
+++ public/scripts/detalle.js
@@ -8,15 +8,15 @@ $(document).ready(async function (){
     <img src="../img/${pieza.id_categoria}.png" class="col-md-4 card ">
     <div class="detalleProducto col-md-7">
     <div class="d-flex align-items-center"><h1 class="fs-1 fw-bold p-2">${pieza.nombre_pieza}</h1>
-    <label id="idPieza" class="text-secondary ms-2" value="${pieza.id}">SKU: ${pieza.id}</label>
+    <label id="idPieza" class="text-secondary ms-2" value="${pieza.id}">SKU: ${pieza.id_pieza}</label>
     </div>
     <div class="col">
     <span id="precio" class="fs-2 text-primary fw-bold p-2" value="${pieza.precio}">$${pieza.precio}.00</span>
     <span class="fs-6 text-info-emphasis bg-body-secondary p-1 ms-3">${pieza.cantidad} en existencia</span>
     </div>
     <div id="modelo&marca" class="d-flex gap-2 align-items-center mb-2">
-    <span class="fs-5"><span class="fw-semibold p-2">Marca:</span>${pieza.nombre_marca}</span>
-    <span class="fs-5"><span class="fw-semibold p-2">Modelo:</span>${pieza.nombre_modelo}</span>
+    <span class="fs-5"><span class="fw-semibold p-2">Marca:</span>${pieza.marca_nombre}</span>
+    <span class="fs-5"><span class="fw-semibold p-2">Modelo:</span>${pieza.modelo_nombre}</span>
     </div>
     <div id="productoDescripcion" class="p-2">
     <div class="fs-5">${pieza.descripcion}</div>
